58d9a6505082826310d89b46917a4f8aebab7543
Martin Johnson <martinjohnson@live.ca>
2020-05-18 16:02:52 -0400

    test Adding a new file

---
+++ UI/Components/DoubleText.cs
@@ -0,0 +1,15 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Util.UiComponents
+{
+    public class TextComponent : ComponentBase
+    {
+        [SerializeField] private TMP_Text text;
+
+        public void SetText(string newText)
+        {
+            text.text = newText;
+        }
+    }
+}
